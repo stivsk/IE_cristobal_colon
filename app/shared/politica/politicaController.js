82fbc9c04bee7ff569e77949c3aa6029438e55b4
stivsk <yeiksite2@gmail.com>
2015-05-21 17:06:34 -0500

    Directorio Re-estructurado

---
+++ app/shared/politica/politicaController.js
@@ -0,0 +1,6 @@
+var politicContent = 
+	{
+		'cont':'La Institución Educativa Cristóbal Colón presta un servicio educativo con personal altamente calificado y una propuesta pedagógica fundamentada en el enfoque socio-crítico que favorezca en los Estudiantes un desempeño integral aplicable a su desempeño cotidiano a través del Proyecto de Vida en donde se destaque la formación en valores y las competencias adquiridas desde un currículo pertinente y estrategias de mejoramiento continuo en favor del crecimiento y transformación personal y social.',
+		'class':'info',
+		'title':'Politica de calidad'
+	};
---
+++ app/shared/valores/valoresController.js
@@ -0,0 +1,26 @@
+var valuesContent = [
+	{
+		'valor':'Reconocimiento',
+		'descrip':'Resaltamos en las personas las acciones y comportamientos que exaltan los valores institucionales.'
+	},
+	{
+		'valor':'Responsabilidad',
+		'descrip':'Educamos individuos que tomen decisiones de manera autónoma, respetando las normas y principios del medio en que interactúan. Cumplimos de manera eficiente y oportuna con los compromisos adquiridos en nuestra cotidianidad.'
+	},
+	{
+		'valor':'Comunicación',
+		'descrip':'Expresamos y escuchamos nuestras ideas, pensamientos y sentimientos desde el valor del respecto. Generamos acciones que favorezcan el diálogo y la comprensión.'
+	},
+	{
+		'valor':'Tolerancia',
+		'descrip':'Reconocemos la individualidad y la diversidad, permitiéndonos establecer vínculos  que posibiliten la convivencia. Construimos lazos sociales basados en la comprensión y en el respeto por la diferencia.'
+	},
+	{
+		'valor':'Honestidad',
+		'descrip':'Actuamos con rectitud dentro de los principios éticos, como condición básica para las relaciones humanas y la auténtica vida comunitaria'
+	},
+	{
+		'valor':'Trabajo en equipo',
+		'descrip':'Las capacidades y esfuerzos individuales se complementan y unifican alrededor de un objetivo común.'
+	}
+];
